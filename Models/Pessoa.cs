f53586405ea92748566b6cab3467cf4302655eb0
Raul Pereira <Raul.pereira1@outlook.com>
2024-03-04 22:00:03 -0300

    começando a configurar o metodo Get usando uma lista estatica para simular um BD que futuramente vai ser implementado.

---
+++ Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿namespace PrimeiraApi.Models
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+
+        public Pessoa(Guid id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+    }
+
+    
+}
---
+++ Rotas/PessoaRotas.cs
@@ -0,0 +1,18 @@
+﻿using PrimeiraApi.Models;
+
+namespace PrimeiraApi.Rotas
+{
+    public static class PessoaRotas
+    {
+        public static List<Pessoa> pessoas = new List<Pessoa>() 
+        { 
+            new Pessoa(id:Guid.NewGuid(),nome:"Raul"),
+            new Pessoa(id:Guid.NewGuid(),nome:"Carla"),
+            new Pessoa(id:Guid.NewGuid(),nome:"Vicenzo")
+        };
+        public static void MapPessoasRotas(this WebApplication app)
+        {
+            app.MapGet("/pessoas", () => pessoas);
+        }
+    }
+}
